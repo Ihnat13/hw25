3509d315ef154aad208a444644bf5049ee142a75
ihnat13 <ignatfomich7@gmail.com>
2024-03-14 19:47:22 +0200

    2

--- src/App.jsx
+++ src/App.jsx
@@ -2,14 +2,12 @@ import React, {Component} from 'react';
 import './App.css';
 import Counter from './Components/counter/counter.jsx';
 
-class App extends Component {
-  render() {
+const App = () => {
     return (
       <div>
         <Counter initialValue={0} />
       </div>
     );
-  }
 }
 
 
