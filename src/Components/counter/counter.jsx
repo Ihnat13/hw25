fb8a7857a3169a787873288e2f6abcc097222615
ihnat13 <ignatfomich7@gmail.com>
2024-03-14 19:38:16 +0200

    1

--- src/Components/counter/counter.jsx
+++ src/Components/counter/counter.jsx
@@ -1,46 +1,32 @@
-import React, { Component } from 'react';
-import './counter.css'
-class Counter extends Component {
-  constructor(props) {
-    super(props);
-    this.state = {
-      count: props.initialValue
-    };
-  }
+import React, { useState } from 'react';
+import counter from './counter.css'
 
-  increment = () => {
-    this.setState(prevState => ({
-      count: prevState.count + 1
-    }));
+const Counter = (props) => {
+  const [count, setCount] = useState(props.initialValue);
+
+  const increment = () => {
+    setCount(prevCount => prevCount + 1);
   };
 
-  decrement = () => {
-    this.setState(prevState => ({
-      count: prevState.count - 1
-    }));
+  const decrement = () => {
+    setCount(prevCount => prevCount - 1);
   };
 
-  reset = () => {
-    this.setState({
-      count: this.props.initialValue
-    });
+  const reset = () => {
+    setCount(props.initialValue);
   };
 
-  render() {
-    return (
-      <div className='counter'>
-        <h2>Counter</h2>
-        <p>Count: {this.state.count}</p>
-        <div className='counter-container'>
-            <button style={{ alignSelf: 'flex-start' }} onClick={this.decrement}>-</button>
-            <button style={{ alignSelf: 'flex-end' }} onClick={this.increment}>+</button>
-        </div>
-        <button onClick={this.reset}>Reset</button>
+  return (
+    <div className='counter'>
+      <h2>Counter</h2>
+      <p>Count: {count}</p>
+      <div className='counter-container'>
+        <button style={{ alignSelf: 'flex-start' }} onClick={decrement}>-</button>
+        <button style={{ alignSelf: 'flex-end' }} onClick={increment}>+</button>
       </div>
-    );
-  }
-}
-
-
+      <button onClick={reset}>Reset</button>
+    </div>
+  );
+};
 
 export default Counter;
